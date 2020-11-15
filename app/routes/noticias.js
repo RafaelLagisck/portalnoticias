e5e1864c703c72144e667abce329e9a822ba79a4
Rafael Lagisck <email@meuemail.com>
2020-11-15 11:48:55 -0300

    Criando Models e Configurando o Consign

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,25 @@
+
 module.exports = function (app){
+
     app.get('/noticias', function(req, res){
-        res.render("noticias/noticias")
+
+        var connection = app.config.dbConnection();
+        var noticiasModel = app.app.models.noticiasModel;
+
+        noticiasModel.getNoticias(connection, function(error, result){
+            res.render("noticias/noticias", {noticias : result});
+        });
+        
+        
+        //Para retornar uma consulta requer a query e um callback
+        //connection.query(<sql>, callback);
+
+        /* jeito antigo
+        connection.query('select *from noticias', function(err, result) {
+            res.render("noticias/noticias", {noticias : result});
+        });
+        */
+        
     });
+    
 };
